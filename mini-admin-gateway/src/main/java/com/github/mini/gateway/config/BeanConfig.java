0044711baa3ee0f138304f90a7dc99bd00244bfe
hansonreal <hanson.qian@foxmail.com>
2022-05-16 14:46:19 +0800

    新增Redis配置 新增Redison配置

--- mini-admin-gateway/src/main/java/com/github/mini/gateway/config/BeanConfig.java
+++ mini-admin-gateway/src/main/java/com/github/mini/gateway/config/BeanConfig.java
@@ -1,8 +1,18 @@
 package com.github.mini.gateway.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.mini.common.util.IdWorker;
+import org.redisson.Redisson;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class BeanConfig {
@@ -12,4 +22,41 @@ public class BeanConfig {
     public IdWorker idWorker(){
         return new IdWorker(1,1);
     }
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Bean
+    public Redisson redisson() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://" + host + ":6379");
+        return (Redisson) Redisson.create(config);
+    }
+
+    @Bean(name = {"redisTemplate", "stringRedisTemplate"})
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
+        StringRedisTemplate redisTemplate = new StringRedisTemplate();
+        // 配置连接工厂
+        redisTemplate.setConnectionFactory(factory);
+        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
+        Jackson2JsonRedisSerializer<Object> jacksonSeial = new Jackson2JsonRedisSerializer<>(Object.class);
+
+        ObjectMapper om = new ObjectMapper();
+        // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        jacksonSeial.setObjectMapper(om);
+
+        // 值采用json序列化
+        redisTemplate.setValueSerializer(jacksonSeial);
+        //使用StringRedisSerializer来序列化和反序列化redis的key值
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        // 设置hash key 和value序列化模式
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(jacksonSeial);
+        redisTemplate.afterPropertiesSet();
+
+        return redisTemplate;
+    }
+
+
 }
